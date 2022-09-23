be33537b9ca561e51e7d8bf52c51aa5c2ed94412
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-09-23 18:33:14 +0900

    Add files via upload

---
+++ property.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	long long int property;
+	long long int saving;
+
+	printf("매달 저축 금액을 입력하시오: ");
+	scanf("%lld", &saving);
+
+	property = saving * 12 * 30;
+	printf("30년 후의 재산 = %lld\n", property);
+
+	return 0;
+}
