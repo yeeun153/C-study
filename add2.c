c014654263f21919b215480013c49610ac17d461
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-09-23 18:04:33 +0900

    Add files via upload

---
+++ add2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int x, y, sum;
+
+	printf("첫 번째 숫자를 입력하시오:");
+	scanf("%d", &x);
+
+	printf("두 번째 숫자를 입력하시오:");
+	scanf("%d", &y);
+
+	sum = x + y;
+
+	printf("두 수의 합=%d\n", sum);
+	return 0;
+}
