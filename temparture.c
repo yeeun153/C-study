ac494fa224b44d2535979c414c96acccb00afa30
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-09-23 18:59:14 +0900

    Add files via upload

---
+++ temparture.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	double celsius, fahrenheit;
+
+	printf("화씨온도=");
+	scanf("%lf", &fahrenheit);
+
+	celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+	printf("섭씨온도=%lf\n", celsius);
+
+	return 0;
+}
