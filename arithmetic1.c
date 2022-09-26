3fc5eafdee7e4544c81957dcd8421fed463deb21
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-09-26 18:03:37 +0900

    Add files via upload

---
+++ arithmetic1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	int x, y, result;
+
+	printf("두 개의 정수를 입력하시오: ");
+	scanf("%d %d", &x, &y);
+
+	result = x + y;
+	printf("%d + %d = %d\n", x, y, result);
+
+	result = x - y;
+	printf("%d - %d = %d\n", x, y, result);
+
+	result = x * y;
+	printf("%d * %d = %d\n", x, y, result);
+
+	result = x / y;
+	printf("%d / %d = %d\n", x, y, result);
+
+	result = x % y;
+	printf("%d %% %d = %d\n", x, y, result);
+
+	return 0;
+}
