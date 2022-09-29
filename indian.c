ff72b3f97742805c0c32d792adf7fbc8ff888f5f
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-09-29 16:14:52 +0900

    Add files via upload

---
+++ indian.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	int init_money = 24;
+	double interest = 0.06;
+	int years = 382;
+
+	double money = init_money * pow((1.0 + interest), years);
+
+	printf("%d년 후의 원리금=%f\n", years, money);
+
+	return 0;
+}
