72df1a8d71a2864e7442e81f45c50aa6e9750d8e
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-02 00:33:51 +0900

    Add files via upload

---
+++ the minimum.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#define SIZE 10
+
+int main(void)
+{
+	int prices[SIZE] = { 12, 3, 19, 6, 18, 8, 12, 4, 1, 19 };
+	int i, minimum;
+
+	printf("[");
+	for (i = 0; i < SIZE; i++) {
+		printf("%d ", prices[i]);
+	}
+	printf("]\n");
+
+	minimum = prices[0];
+
+	for (i = 1; i < SIZE; i++)
+	{
+		if (prices[i] < minimum)
+			minimum = prices[i];
+	}
+	printf("최솟값은 %d입니다.\n", minimum);
+	return 0;
+}
