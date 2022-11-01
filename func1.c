7af8a460b38432c081ee14cbcf5e1fd4594e38e6
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-02 01:21:50 +0900

    Add files via upload

---
+++ func1.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int get_integer()
+{
+	int value;
+	printf("정수를 입력하시오 : ");
+	scanf("%d", &value);
+	return value;
+}
+
+int main(void)
+{
+	int x = get_integer();
+	int y = get_integer();
+	int result = x + y;
+	printf("두 수의 합 = %d \n", result);
+
+	return 0;
+}
