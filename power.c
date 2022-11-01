1f550be1f020d85d1f0ca4e55ca91991e8f080a3
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-02 01:31:03 +0900

    Add files via upload

---
+++ power.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int get_integer()
+{
+	int value;
+	printf("정수를 입력하시오 : ");
+	scanf("%d", &value);
+	return value;
+}
+
+int power(int x, int y)
+{
+	int i;
+	long result = 1;
+
+	for (i = 0; i < y; i++)
+		result *= x;
+
+	return result;
+}
+
+int main(void)
+{
+	int x = get_integer();
+	int y = get_integer();
+	int result = power(x, y);
+	printf("%d의 %d승 = %d \n", x, y, result);
+
+	return 0;
+}
