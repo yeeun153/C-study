b4cb50afd0a544296db705afa5e37b39bd7d4633
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-02 01:26:16 +0900

    Add files via upload

---
+++ get_max.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int get_integer()
+{
+	int value;
+	printf("정수를 입력하시오 : ");
+	scanf("%d", &value);
+	return value;
+}
+
+int get_max(int x, int y)
+{
+	if (x > y)return(x);
+	else return(y);
+}
+
+int main(void)
+{
+	int a = get_integer();
+	int b = get_integer();
+
+	printf("두 수 중에서 큰 수는 %d입니다.\n", get_max(a, b));
+	return 0;
+}
