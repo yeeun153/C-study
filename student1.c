7b4f833246637ef2766348cfa39670a5d5f38f1a
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 01:31:08 +0900

    Add files via upload

---
+++ student1.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+struct student {
+	int number;
+	char name[10];
+	double grade;
+};
+
+int main(void)
+{
+	struct student s;
+
+	s.number = 2021111335;
+	strcpy(s.name, "손예은");
+	s.grade = 4.5;
+
+	printf("학번: %d\n", s.number);
+	printf("이름 :%s\n", s.name);
+	printf("학점 :%f\n", s.grade);
+
+	return 0;
+}
