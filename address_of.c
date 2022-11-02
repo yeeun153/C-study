d7e8ba06f991c0d8fb9b394a7e4017d8d1f6cc84
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-02 19:48:03 +0900

    Add files via upload

---
+++ address_of.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i = 10;
+	char c = 69;
+	double f = 12.3;
+
+	printf("i의 주소: %u\n", &i);
+	printf("c의 주소: %u\n", &c);
+	printf("f의 주소: %u\n", &f);
+
+	return 0;
+}
