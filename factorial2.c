bad5d16362c7642d66c78f94c7f60f61c279a639
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-02 19:32:38 +0900

    Add files via upload

---
+++ factorial2.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+long factorial(int n);
+
+int main(void)
+{
+	int x = 0, result;
+
+	printf("정수를 입력하시오:");
+	scanf("%d", &x);
+
+	result = factorial(x);
+	printf("%d!은 %d입니다.\n", x, result);
+
+	return 0;
+}
+
+int factorial(int n)
+{
+	printf("factorial(%d)\n", n);
+
+	if (n <= 1)return 1;
+	else return n * factorial(n - 1);
+}
