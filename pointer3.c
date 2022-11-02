8fcf754322dc7a426927ef7ab39381647d0f19b0
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 01:09:51 +0900

    Add files via upload

---
+++ pointer3.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	char s[] = "HelloWorld";
+	char* p = "HelloWorld";
+
+	s[0] = 'h';
+
+	printf("포인터가 가리키는 문자열 = %s \n", p);
+	p = "Goodbye";
+	printf("포인터가 가리키는 문자열 = %s \n", p);
+	return 0;
+}
