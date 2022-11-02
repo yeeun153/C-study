fa301f93ac0aeb40a5d60be5a9b6e464deb1e1a2
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 01:06:13 +0900

    Add files via upload

---
+++ strcmp.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char s1[80];
+	char s2[80];
+	int result;
+
+	printf("첫 번째 단어를 입력하시오:");
+	scanf("%s", s1);
+	printf("두 번째 단어를 입력하시오:");
+	scanf("%s", s2);
+
+	result = strcmp(s1, s2);
+	if (result < 0)
+		printf("%s가 %s보다 앞에 있습니다.\n", s1, s2);
+	else if (result == 0)
+		printf("%s가 %s와 같습니다.\n", s1, s2);
+	else
+		printf("%s가 %s보다 뒤에 있습니다.\n", s1, s2);
+
+	return 0;
+}
