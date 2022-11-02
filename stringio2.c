966e49ce0b1541736aa5422f592f7da91757622d
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-02 20:53:03 +0900

    Add files via upload

---
+++ stringio2.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	char name[100];
+	char address[100];
+
+	printf("이름이 어떻게 되시나요? ");
+	gets_s(name, 99);
+
+	printf("어디 사시나요?  ");
+	gets_s(address, 99);
+
+	printf("안녕하세요, %s에 사는 %s씨.\n", address, name);
+	return 0;
+}
