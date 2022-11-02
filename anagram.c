5907db31488143d2af71f4cb2c2f00071d6f6da6
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-02 21:09:06 +0900

    Add files via upload

---
+++ anagram.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#define SOL "apple"
+
+int main(void)
+{
+	char s[100] = SOL;
+	char ans[100];
+	int i, len;
+
+	len = strlen(s);
+	for (i = 0; i < len; i++) {
+		int pos1 = rand() % len;
+		int pos2 = rand() % len;
+		char tmp = s[pos1];
+		s[pos1] = s[pos2];
+		s[pos2] = tmp;
+	}
+	do {
+		printf("%s는 어떤 단어가 스크램블된 것일까요?", s);
+		scanf("%s", ans);
+	} while (strcmp(ans, SOL) != 0);
+
+	printf("축하합니다.\n");
+	return 0;
+}
