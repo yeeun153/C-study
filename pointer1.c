340843f9873fcf7dcdfc09966875afe526cd89aa
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-02 19:51:48 +0900

    Add files via upload

---
+++ pointer1.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int number = 10;
+	int* p;
+
+	p = &number;
+	
+	printf("변수 number의 주소 = %u\n", &number);
+	printf("포인터의 값 = %u\n", p);
+	printf("변수 number의 주소 = %d\n", number);
+	printf("포인터의 값 = %d\n", *p);
+
+	return 0;
+
+}
