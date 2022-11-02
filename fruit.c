a86993372d52257e8716854651dce7b0be2434c6
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 01:13:31 +0900

    Add files via upload

---
+++ fruit.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char key[] = "사과";
+	char buffer[80];
+
+	do {
+		printf("내가 제일 좋아하는 과일은 무엇일까요? ");
+		scanf("%s", buffer);
+	} while (strcmp(key, buffer) != 0);
+
+	printf("맞았습니다!");
+	return 0;
+}
