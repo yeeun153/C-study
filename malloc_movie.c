12ed1096ba6474c812306b6c11ad62981e708b23
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 22:01:08 +0900

    Add files via upload

---
+++ malloc_movie.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+struct movie
+{
+	char title[100];
+	double rating;
+};
+
+int main(void)
+{
+	struct movie* ptr;
+	int i, n;
+
+	printf("영화의 개수:");
+	scanf("%d", &n);
+
+	ptr = (struct moive*)
+		malloc(n * sizeof(struct movie));
+
+	if (ptr == NULL) {
+		printf("메모리 할당 오류\n");
+		exit(1);
+	}
+	for (i = 0; i < n; i++) {
+		printf("영화 제목:");
+		scanf("%s", ptr[i].title);
+		printf("영화 평점:");
+		scanf("%lf", &ptr[i].rating);
+	}
+
+	printf("\n=========================\n");
+	for (i = 0; i < n; i++) {
+		printf("영화 제목:%s\n", ptr[i].title);
+		printf("영화 평점:%lf\n", ptr[i].rating);
+	}
+	printf("\n=========================\n");
+	free(ptr);
+	return 0;
+}
