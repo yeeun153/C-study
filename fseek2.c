455e4a1de404e2bba7fae6172fa713f2ffb4b901
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 21:36:23 +0900

    Add files via upload

---
+++ fseek2.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp;
+	fp = fopen("data.txt", "w");
+	if (fp == NULL) {
+		printf("data.txt 파일을 열 수 없습니다.");
+		exit(1);
+	}
+	fputs("This is an house.", fp);
+	fseek(fp, 11, SEEK_SET);
+	fputs("apple", fp);
+	fclose(fp);
+
+	return 0;
+}
