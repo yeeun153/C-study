f32949704a2f1ebb640f7d8f9abbf8095fa4d176
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 10:28:00 +0900

    Add files via upload

---
+++ pointer_to_st.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+struct student {
+	int number;
+	char name[20];
+	double grade;
+};
+
+int main(void)
+{
+	struct student s = { 2021111335, "손예은",4.5 };
+	struct student* p;
+
+	p = &s;
+
+	printf("학번=%d 이름=%s 학점=%f \n", s.number, s.name, s.grade);
+	printf("학번=%d 이름=%s 학점=%f \n", (*p).number, (*p).name, (*p).grade);
+	printf("학번=%d 이름=%s 학점=%f \n", p->number, p->name, p->grade);
+
+	return 0;
+}
