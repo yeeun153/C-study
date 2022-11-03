b69fe25213ba5cf7f09967244300896fb4a85c4d
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 11:00:12 +0900

    Add files via upload

---
+++ fgetc.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp = NULL;
+	int c;
+
+	fp = fopen("alphabet.txt", "w");
+	if (fp == NULL) {
+		fprintf(stderr, "파일 alphabet.txt를 열 수 없습니다.\n");
+		exit(1);
+	}
+
+	while ((c = fgetc(fp)) != EOF)
+		putchar(c);
+
+	fclose(fp);
+	return 0;
+}
