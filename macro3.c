57f40a9037d9dd8ce54e568dee6d87c5040073c6
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 23:01:39 +0900

    Add files via upload

---
+++ macro3.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#define SETBIT(x,n) ((x) |= (1<<(n)))
+#define CLEARBIT(x,n) ((x) &= ~(1<<(n)))
+#define TESTBIT(x,n) ((x) & (1<<(n)))
+
+int main(void)
+{
+	int x = 0x1011;
+
+	printf("SETBIT(x, 8)전 변수x = %x\n", x);
+	SETBIT(x, 8);
+	printf("SETBIT(x, 8)후 변수x = %x\n", x);
+
+	printf("CLEARBIT(x, 8)전 변수x = %x\n", x);
+	CLEARBIT(x, 8);
+	printf("CLEARBIT(x, 8)후 변수x = %x\n", x);
+
+	return 0;
+};
