0c543617d2144f122e3196e83b2662243378b851
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 21:26:14 +0900

    Add files via upload

---
+++ score4.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp;
+	int number, count = 0;
+	char name[20];
+	float score, total = 0.0;
+
+	if ((fp = fopen("scores.txt", "r")) == NULL)
+	{
+		fprintf(stderr, "성적 파일을 열 수 없습니다.\n");
+		exit(1);
+	}
+	while (!feof(fp))
+	{
+		fscanf(fp, "%d %s %f", &number, name, &score);
+		printf("%d %s %f\n", number, name, score);
+		total += score;
+		count++;
+	}
+	printf("평균=%f\n", total / count);
+	fclose(fp);
+	return 0;
+}
