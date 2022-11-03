b9fab3a5b8ff37624523cbc4d52f054f01715112
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 21:14:04 +0900

    Add files via upload

---
+++ fprintf.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE* fp;
+
+	fp = fopen("sales.txt", "a");
+	fprintf(fp, "2017.3.1 매출 : %d \n", 200000);
+
+	fclose(fp);
+	return 0;
+}
