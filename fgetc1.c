199e5e86890bae1253344d3e3d93fcdc6ccf219a
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 21:06:45 +0900

    Add files via upload

---
+++ fgetc1.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp;
+	char str[100];
+
+	fp = fopen("file.txt", "w");
+
+	if (fp == NULL) {
+		fprintf("stderr", "파일 file.txt를 열 수 없습니다.\n");
+		exit(0);
+	}
+
+	do {
+		gets(str);
+		fputs(str, fp);
+	} while (strlen(str) != 0);
+
+	fclose(fp);
+	return 0;
+}
