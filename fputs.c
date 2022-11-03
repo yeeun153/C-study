e53ea7e68a1cf75cacaf62b7e2f0489c4c2f2317
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-03 10:52:40 +0900

    Add files via upload

---
+++ fputs.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp = NULL;
+
+	fp = fopen("alphabet.txt", "w");
+	if (fp == NULL) {
+		fprintf(stderr, "파일 alphabet.txt를 열 수 없습니다.\n");
+		exit(1);
+	}
+
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		fputc(c, fp);
+
+	fclose(fp);
+	return 0;
+}
